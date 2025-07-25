1b1a10fa83b1012977da11865a9e0326af7147ad
Fulgence <fulgenceondoro@gmail.com>
2025-07-25 23:56:07 +0300

    Implementing User Authentication in Django

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,15 @@
+from atexit import register
+
+from django.contrib.auth.views import LoginView
 from django.urls import path
-from .views import list_books, LibraryDetailView
+from pyasn1_modules.rfc7906 import Register
+
+from .views import list_books, LibraryDetailView, LogoutView, RegisterView, login_view, logout_view, register_view
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView, name='library_detail'),
+    path('login/', login_view,  name='login'),
+    path('logout/', logout_view, name='logout'),
+    path('register/', register_view, name='register'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,10 +1,13 @@
 from django.views.generic.detail import DetailView
 from aiohttp.web_urldispatcher import View
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Book
 from .models import Library
 from .models import Librarian
 from .models import Author
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, PasswordChangeForm
 
 # Create your views here.
 from django.http import JsonResponse
@@ -18,3 +21,51 @@ class LibraryDetailView(View):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
+#User registration view
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect(list_books)
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
+
+# User login view
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect(list_books)
+    else:
+        form = AuthenticationForm()
+    return render(request, 'relationship_app/login.html', {'form': form})
+
+
+# Logout
+
+@login_required
+def logout_view(request):
+    logout(request)
+    return render(request, 'relationship_app/logout.html')
+
+
+# Password change
+
+@login_required
+def password_change(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(data=request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return render(request, 'relationship_app/password_change.html')
+    else:
+        form = PasswordChangeForm()
+    return render(request, 'relationship_app/password_change.html', {'form': form})
