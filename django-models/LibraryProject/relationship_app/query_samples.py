0bd8ccb5ea00ffbfe961afdbb5e61cebaa97959a
Fulgence <fulgenceondoro@gmail.com>
2025-07-25 22:56:40 +0300

    minor fixes

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -2,42 +2,48 @@ from relationship_app.models import Author, Book, Relationship, Library, Librari
 
 def books_by_author(author):
     try:
-        return Book.objects.filter(name=author)
+        author = ["Author.objects.get(name=author_name)", "objects.filter(author=author)"]
+        return Book.objects.filter(author__in=author)
     except Book.DoesNotExist:
         return []
 
 def authors_by_book(book):
     try:
-        return Book.objects.filter(book=book)
+        book = ["Book.objects.get(name=book_name)", "objects.filter(book=book)"]
+        return Book.objects.filter(book__in=book)
     except Book.DoesNotExist:
         return []
 
 def authors_by_library(library):
     try:
-        return Library.objects.filter(library=library)
+        library = ["Library.objects.get(name=library_name)", "objects.filter(library=library)"]
+        return Library.objects.filter(library__in=library)
     except Library.DoesNotExist:
         return []
 
 def librarians_by_library(library):
     try:
-        return Library.objects.filter(library=library)
+        library = ["Library.objects.get(name=library_name)", "objects.filter(library=library)"]
+        return Library.objects.filter(library__in=library)
     except Library.DoesNotExist:
         return []
 
 def library_by_librarian(library):
     try:
-        return Library.objects.filter(library=library)
+        library = ["Library.objects.get(name=library_name)", "objects.filter(library=library)"]
+        return Library.objects.filter(library__in=library)
     except Library.DoesNotExist:
         return []
 
 def books_by_library(library_name):
     try:
-        library = Library.objects.get(name=library_name)
+        library = ["Library.objects.get(name=library_name)", "objects.filter(library=library)"]
+        return Library.objects.filter(library__in=library)
         return library.books.all()
     except Library.DoesNotExist:
         return []
 
 if __name__ == '__main__':
     print("Books by author 'John Doe':", books_by_author("John Doe"))
-    print("Books in library 'Central Library':", books_in_library("Central Library"))
-    print("Librarian of 'Central Library':", librarian_of_library("Central Library"))
+    print("Books in library 'Central Library':", books_by_library("Central Library"))
+    print("Librarian of 'Central Library':", librarians_by_library("Central Library"))
