a2116e552cbc82772d3be8b7ef83db2cbff1577c
Fulgence <fulgenceondoro@gmail.com>
2025-07-30 17:39:34 +0300

    0. Implementing a Custom User Model in Django

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,4 +1,9 @@
 from django.db import models
+from django.conf import settings
+
+class MyModel(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
 
 # Create your models here.
 class Book(models.Model):
@@ -8,3 +13,25 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, username, password=None):
+        user = self.model(email=self.normalize_email(email), username=username)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    def create_superuser(self, email, username, password):
+        user = self.create_user(email, username, password)
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(blank=True, null=True)
+    profile_photo = models.ImageField(upload_to="profile_photos/", null=True, blank=True)
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.username
