d97cfba3f718a612af0685ce657ead3f7bc60fb9
Fulgence <fulgenceondoro@gmail.com>
2025-07-30 12:38:55 +0300

    updates

---
+++ advanced_features_and_security/LibraryProject/LibraryProject/relationship_app/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager, AbstractUser
+
+
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to="profile_photos/", null=True, blank=True)
+
+    def __str__(self):
+        return self.username  # AbstractUser already includes username
+
+
+# Create your models here.
+class Relationship(models.Model):
+    name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author =models.ForeignKey(Author, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.title
+
+class Library(models.Model):
+    name = models.CharField(max_length=100)
+    books = models.ManyToManyField(Book)
+    def __str__(self):
+        return self.name
+
+class Librarian(models.Model):
+    name = models.CharField(max_length=100)
+    library = models.ForeignKey(Library, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
