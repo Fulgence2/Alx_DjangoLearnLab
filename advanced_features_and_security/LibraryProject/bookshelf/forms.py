2e748dffc3f618f14136508b8099cf4e2bf84e7d
Fulgence <fulgenceondoro@gmail.com>
2025-08-03 21:39:13 +0300

    2. Implementing Security Best Practices in Django

---
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from .models import Book
+
+class BookForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ['title', 'author', 'description']
+
+class SearchForm(forms.Form):
+    q = forms.CharField(
+        max_length=100,
+        required=False,
+        widget=forms.TextInput(attrs={'placeholder': 'Search for books...'})
+    )
