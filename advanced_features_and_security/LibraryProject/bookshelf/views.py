e203a514ae1632428342895de7582743971e29f8
Fulgence <fulgenceondoro@gmail.com>
2025-08-03 21:44:10 +0300

    2. Implementing Security Best Practices in Django

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .forms import ExampleForm
 
 # Create your views here.
 # bookshelf/views.py
@@ -11,4 +12,14 @@ from .models import Book
 def book_list(request):
     books = Book.objects.all()
     return render(request, 'bookshelf/book_list.html', {'books': books})
+
+def add_book(request):
+    if request.method == 'POST':
+        form = ExampleForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = ExampleForm()
+    return render(request, 'bookshelf/form_example.html', {'form': form})
 
