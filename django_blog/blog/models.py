08830f47d2a250dcea0f9bf04ff7f678d76f82f5
Fulgence <fulgenceondoro@gmail.com>
2025-08-22 22:02:19 +0300

    0. Initial Setup and Project Configuration for a Django Blog

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -7,6 +7,6 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    date = models.DateTimeField(auto_now_add=True)
+    published_date = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return self.title
