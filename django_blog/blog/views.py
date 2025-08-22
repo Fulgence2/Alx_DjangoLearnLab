776f403cb01a88956bf6c5c907afe15a6197df8b
Fulgence <fulgenceondoro@gmail.com>
2025-08-23 00:33:32 +0300

    4. Implementing Advanced Features: Tagging and Search Functionality

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -13,10 +13,7 @@ from django.db.models import Q
 
 def search_posts(request):
     query = request.GET.get('q', "")
-    results = Post.objects.all()
-
-    if query:
-        results = results.filter(
+    results = Post.objects.filter(
             Q(title__icontains=query) |
             Q(content__icontains=query) |
             Q(tag__name__icontains=query)
