4a6b3218a5580338c0ed94a0b5dba6041a100e38
Fulgence <fulgenceondoro@gmail.com>
2025-08-22 23:39:16 +0300

    2. Creating Blog Post Management Features

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -16,6 +16,7 @@ class PostListView(ListView):
     context_object_name = 'posts'
     paginate_by = 10
     template_name = 'blog/post_list.html'
+
 class PostDetailView(DetailView):
     model = Post
     context_object_name = 'post'
@@ -40,6 +41,9 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     def test_func(self):
         post = self.get_object()
         return self.request.user == post.author
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
